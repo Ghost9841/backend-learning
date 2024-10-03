40eff6d2a6dcafe4e0969ebfee756fd94998e35f
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2024-10-03 18:32:30 +0545

    Add files via upload

--- web_server_01/server-node.js
+++ web_server_01/server-node.js
@@ -1,1 +1,24 @@
+const http = require('http')
+const hostname = '127.0.0.1'
+const port = 3000
 
+const server = http.createServer((req,res) =>{
+    if (req.url === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end("Hello World")
+    }
+    else if (req.url === '/world'){
+        res.statusCode = 200
+        res.setHeader('Content-Type','text/plain')
+        res.end("Why you searching for world? You got me. Your Whole World is me :wink:")
+    }
+    else{
+        res.statusCode = 404
+        res.setHeader("Content-Type",('text/plain'))
+        res.end("404 : The world you are looking for not found ")
+    }
+})
+server.listen (port,hostname,() => {
+    console.log(`Server is listening at http://${hostname}:${port}`)
+})
